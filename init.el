cb1b56b16e84273e30ca7ffb81d0ee37e36dc6df
Étienne BERSAC <bersace03@gmail.com>
2015-02-28 10:40:54 +0100

    Abandonner MELPA

--- init.el
+++ init.el
@@ -47,8 +47,6 @@
 ;; Configuration des dépôts de paquets emacs
 (require 'package)
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives
 	     '("elpy" . "http://jorgenschaefer.github.io/packages/") t)
 
 ;; Installation des dépendances de paquets emacs
